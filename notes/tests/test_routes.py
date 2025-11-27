8fb0b17f273d3911421f396fd624e871bb588dd8
dotsenkois <dotsenkois@yandex.ru>
2025-11-27 12:35:23 +0700

    Добавлено тестирование доступности страницы заметки

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -2,17 +2,52 @@
 # Импортируем класс HTTPStatus.
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 # Импортируем функцию reverse().
 from django.urls import reverse
 
+from notes.models import Note
+
+TEST_USER = 'testuser'
+TEST_PASSWORD = 'password123'
 
 class TestRoutes(TestCase):
 
-    def test_home_page(self):
-        # Вместо прямого указания адреса
-        # получаем его при помощи функции reverse().
-        url = reverse('notes:home')
+    @classmethod
+    def setUpTestData(cls):
+        # Создаём пользователя
+        cls.user = get_user_model().objects.create_user(
+            username=TEST_USER,
+            password=TEST_PASSWORD
+        )
+
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            text='Текст',
+            slug='test-slug',
+            author=cls.user,
+        )
+
+
+    def test_pages_availability(self):
+        urls = (
+            ('notes:home', None),
+        )
+
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_detail_page_as_author(self):
+        # Логинимся под созданным пользователем
+        self.client.login(username=TEST_USER, password=TEST_PASSWORD)
+
+        url = reverse('notes:detail', args=(self.note.slug,))
         response = self.client.get(url)
-        # Проверяем, что код ответа равен статусу OK (он же 200).
+
         self.assertEqual(response.status_code, HTTPStatus.OK)
+        # Дополнительно можно проверить, что в контексте вернулась именно эта заметка
+        self.assertEqual(response.context['note'], self.note)
