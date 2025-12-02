e21b682ecce93f60dbb79771a22df4d360de5bb1
dotsenkois <dotsenkois@yandex.ru>
2025-12-02 08:53:06 +0700

    Дообавлена параметрзация

--- notes/pytest_tests/test_content.py
+++ notes/pytest_tests/test_content.py
@@ -1,25 +1,25 @@
 # test_content.py
+import pytest
+
 from django.urls import reverse
 
-# В тесте используем фикстуру заметки
-# и фикстуру клиента с автором заметки.
-def test_note_in_list_for_author(note, author_client):
-    url = reverse('notes:list')
-    # Запрашиваем страницу со списком заметок:
-    response = author_client.get(url)
-    # Получаем список объектов из контекста:
-    object_list = response.context['object_list']
-    # Проверяем, что заметка находится в этом списке:
-    assert note in object_list
 
-
-# В этом тесте тоже используем фикстуру заметки,
-# но в качестве клиента используем not_author_client;
-# в этом клиенте авторизован не автор заметки, 
-# так что заметка не должна быть ему видна.
-def test_note_not_in_list_for_another_user(note, not_author_client):
+@pytest.mark.parametrize(
+    # Задаём названия для параметров:
+    'parametrized_client, note_in_list',
+    (
+        # Передаём фикстуры в параметры при помощи "ленивых фикстур":
+        (pytest.lazy_fixture('author_client'), True),
+        (pytest.lazy_fixture('not_author_client'), False),
+    )
+)
+# Используем фикстуру заметки и параметры из декоратора:
+def test_notes_list_for_different_users(
+    note, parametrized_client, note_in_list
+):
     url = reverse('notes:list')
-    response = not_author_client.get(url)
+    # Выполняем запрос от имени параметризованного клиента:
+    response = parametrized_client.get(url)
     object_list = response.context['object_list']
-    # Проверяем, что заметки нет в контексте страницы:
-    assert note not in object_list
+    # Проверяем истинность утверждения "заметка есть в списке":
+    assert (note in object_list) is note_in_list
