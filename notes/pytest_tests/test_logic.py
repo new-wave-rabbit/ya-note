a37952d37f9d06d9e953b332885fa1886091bae3
dotsenkois <dotsenkois@yandex.ru>
2025-12-02 08:58:46 +0700

    test_logic.py initial

---
+++ notes/pytest_tests/test_logic.py
@@ -0,0 +1,29 @@
+# test_logic.py
+from pytest_django.asserts import assertRedirects
+
+from django.urls import reverse
+
+from notes.models import Note
+
+
+# Указываем фикстуру form_data в параметрах теста.
+def test_user_can_create_note(author_client, author, form_data):
+    url = reverse('notes:add')
+    # В POST-запросе отправляем данные, полученные из фикстуры form_data:
+    response = author_client.post(url, data=form_data)
+    # Проверяем, что был выполнен редирект на страницу успешного добавления заметки:
+    assertRedirects(response, reverse('notes:success'))
+    # Считаем общее количество заметок в БД, ожидаем 1 заметку.
+    assert Note.objects.count() == 1
+    # Чтобы проверить значения полей заметки - 
+    # получаем её из базы при помощи метода get():
+    new_note = Note.objects.get()
+    # Сверяем атрибуты объекта с ожидаемыми.
+    assert new_note.title == form_data['title']
+    assert new_note.text == form_data['text']
+    assert new_note.slug == form_data['slug']
+    assert new_note.author == author
+    # Вроде бы здесь нарушен принцип "один тест - одна проверка";
+    # но если хоть одна из этих проверок провалится - 
+    # весь тест можно признать провалившимся, а последующие невыполненные проверки
+    # не внесли бы в отчёт о тесте ничего принципиально важного.
