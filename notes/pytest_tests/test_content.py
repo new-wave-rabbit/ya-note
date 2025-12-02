41e047c163fc4faa22b144f62e7f1758a50fa564
dotsenkois <dotsenkois@yandex.ru>
2025-12-02 08:55:52 +0700

    Проверка наличия формы

--- notes/pytest_tests/test_content.py
+++ notes/pytest_tests/test_content.py
@@ -2,7 +2,8 @@
 import pytest
 
 from django.urls import reverse
-
+# Импортируем класс формы.
+from notes.forms import NoteForm
 
 @pytest.mark.parametrize(
     # Задаём названия для параметров:
@@ -23,3 +24,24 @@ def test_notes_list_for_different_users(
     object_list = response.context['object_list']
     # Проверяем истинность утверждения "заметка есть в списке":
     assert (note in object_list) is note_in_list
+
+
+def test_create_note_page_contains_form(author_client):
+    url = reverse('notes:add')
+    # Запрашиваем страницу создания заметки:
+    response = author_client.get(url)
+    # Проверяем, есть ли объект form в словаре контекста:
+    assert 'form' in response.context
+    # Проверяем, что объект формы относится к нужному классу.
+    assert isinstance(response.context['form'], NoteForm)
+
+
+# В параметры теста передаём фикстуру slug_for_args и клиент с автором заметки:
+def test_edit_note_page_contains_form(slug_for_args, author_client):
+    url = reverse('notes:edit', args=slug_for_args)
+    # Запрашиваем страницу редактирования заметки:
+    response = author_client.get(url)
+    # Проверяем, есть ли объект form в словаре контекста:
+    assert 'form' in response.context
+    # Проверяем, что объект формы относится к нужному классу.
+    assert isinstance(response.context['form'], NoteForm) 
